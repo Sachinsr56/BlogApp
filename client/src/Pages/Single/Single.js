a046c74e39cf3bd434949f93912acce944261030
Sachinsr56 <sachinkumarhas@gmail.com>
2023-03-15 15:56:35 +0530

    Single Post Components

---
+++ client/src/Pages/Single/Single.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from '../../Components/Sidebar/SideBar'
+import SinglePost from '../../Components/SinglePost/SinglePost'
+import './Single.css'
+const Single = () => {
+  return (
+    <div className='single'>
+        <SinglePost/>
+        <Sidebar/>
+    </div>
+  )
+}
+
+export default Single
