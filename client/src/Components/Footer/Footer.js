fca808520ed8e08e5b609f83af5e6b4482a164cc
Sachinsr56 <sachinkumarhas@gmail.com>
2023-03-15 10:29:17 +0530

    Footer Completed

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -1,61 +1,62 @@
 import React from "react";
-
+import './Footer.css'
 const Footer = () => {
   return (
-    <div>
+    <div id="Footer">
       <footer class="bg-light text-center text-white">
         <div class="container">
           <section>
             <a
-              class="btn text-white btn-floating m-1"
+              className="btn text-white btn-floating m-1"
+              style={{ "background-color": "#ac2bac" }}
+              href="https://www.instagram.com/ksachin___/"
+              role="button"
+            >
+              <i className="fab fa-instagram "></i>
+            </a>
+
+            <a
+              className="btn text-white btn-floating m-1"
+              style={{ "background-color": "#0082ca" }}
+              href="https://www.linkedin.com/in/sachinsr56/"
+              role="button"
+            >
+              <i className="fab fa-linkedin-in "></i>
+            </a>
+
+            <a
+              className="btn text-white btn-floating m-1"
+              style={{ "background-color": "#333333" }}
+              href="https://github.com/Sachinsr56"
+              role="button"
+            >
+              <i className="fab fa-github "></i>
+            </a>
+            <a
+              className="btn text-white btn-floating m-1"
               style={{ "background-color": "#3b5998" }}
               href="#!"
               role="button"
             >
-              <i class="fab fa-facebook-f"></i>
+              <i className="fab fa-facebook-f "></i>
             </a>
 
             <a
-              class="btn text-white btn-floating m-1"
+              className="btn text-white btn-floating m-1"
               style={{ "background-color": "#55acee" }}
               href="#!"
               role="button"
             >
-              <i class="fab fa-twitter"></i>
+              <i className="fab fa-twitter "></i>
             </a>
 
             <a
-              class="btn text-white btn-floating m-1"
+              className="btn text-white btn-floating m-1"
               style={{ "background-color": "#dd4b39" }}
               href="#!"
               role="button"
             >
-              <i class="fab fa-google"></i>
-            </a>
-            <a
-              class="btn text-white btn-floating m-1"
-              style={{ "background-color": "#ac2bac" }}
-              href="#!"
-              role="button"
-            >
-              <i class="fab fa-instagram"></i>
-            </a>
-
-            <a
-              class="btn text-white btn-floating m-1"
-              style={{ "background-color": "#0082ca" }}
-              href="#!"
-              role="button"
-            >
-              <i class="fab fa-linkedin-in"></i>
-            </a>
-            <a
-              class="btn text-white btn-floating m-1"
-              style={{ "background-color": "#333333" }}
-              href="#!"
-              role="button"
-            >
-              <i class="fab fa-github"></i>
+              <i className="fab fa-google "></i>
             </a>
           </section>
         </div>
